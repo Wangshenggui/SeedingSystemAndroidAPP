c1277a947943929be0dd5bc8bac5c97fd3ca8775
Mac <1785193871@qq.com>
2024-06-28 22:07:27 +0800

    创建了播种界面

--- app/src/main/java/Seeding/System/MainActivity.java
+++ app/src/main/java/Seeding/System/MainActivity.java
@@ -1,6 +1,10 @@
 package Seeding.System;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //跳转到电机控制页面
+    Button goMotConActivityButton;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //获取控件ID
+        goMotConActivityButton = (Button) findViewById(R.id.goMotConActivityButton);
+        goMotConActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MotorControlActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
---
+++ app/src/main/java/Seeding/System/MotorControlActivity.java
@@ -0,0 +1,19 @@
+package Seeding.System;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MotorControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motor_control);
+
+    }
+}
